374a367764bebbbcbea6902444b98584a95d7883
jamminP <parkjaemin08@gmail.com>
2024-06-07 05:25:20 +0900

    알람 생성자 추가

--- src/main/java/com/example/demo/event/AlarmMessage.java
+++ src/main/java/com/example/demo/event/AlarmMessage.java
@@ -1,6 +1,5 @@
 package com.example.demo.event;
 
-
 import com.example.demo.entity.users.Alarm.AlarmArgs;
 import com.example.demo.entity.users.Alarm.AlarmEntity;
 import com.example.demo.entity.users.Alarm.AlarmType;
@@ -13,19 +12,30 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 public class AlarmMessage {
+    private Long id; // 추가
     private String receiveUserId;
     private AlarmType alarmType;
     private AlarmArgs alarmArgs;
     private LocalDateTime createdAt;
     private String text;
 
+    // 새 생성자 추가
+    public AlarmMessage(String receiveUserId, AlarmType alarmType, AlarmArgs alarmArgs, LocalDateTime createdAt, String text) {
+        this.receiveUserId = receiveUserId;
+        this.alarmType = alarmType;
+        this.alarmArgs = alarmArgs;
+        this.createdAt = createdAt;
+        this.text = text;
+    }
+
     public static AlarmMessage fromEntity(AlarmEntity entity){
         return new AlarmMessage(
+                entity.getId(), // 추가
                 entity.getUser().getUserId(),
                 entity.getAlarmType(),
                 entity.getArgs(),
-                entity.getCreatedAt()
-                ,entity.getText()
+                entity.getCreatedAt(),
+                entity.getText()
         );
     }
 }
