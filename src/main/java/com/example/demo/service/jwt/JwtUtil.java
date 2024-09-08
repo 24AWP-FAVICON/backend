417d3b7bcc1b307236cdb74f900f240476e8e7f5
YoonYn9915 <jsy440720@gmail.com>
2024-09-08 22:03:49 +0900

    #34-refactor: Access Token으로 Authentication 객체 만들어 반환하는 메서드 추가

--- src/main/java/com/example/demo/service/jwt/JwtUtil.java
+++ src/main/java/com/example/demo/service/jwt/JwtUtil.java
@@ -1,21 +1,30 @@
 package com.example.demo.service.jwt;
 
+import com.example.demo.entity.users.user.User;
 import com.example.demo.exception.ComponentNotFoundException;
 import com.example.demo.service.RedisUtil;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import com.example.demo.repository.users.user.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
+import java.util.Collections;
 import java.util.Date;
+import java.util.List;
 
 
+@Slf4j
 @Component
 public class JwtUtil {
 
@@ -118,6 +127,24 @@ public class JwtUtil {
         }
 
         return refreshToken;
+    }
+
+    /**
+     * Access Token 으로 인증객체를 만드는 메서드
+     */
+    public Authentication getAuthentication(String token) {
+
+        Claims claims = Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload();
+
+        String userId = claims.get("userId", String.class);
+        log.info("Request By {}", userId);
+
+        // 권한 정보를 포함하지 않음. 권한이 필요한 경우 기본 ROLE_USER로 설정
+        List<SimpleGrantedAuthority> authorities = Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"));
+
+        User principal = userRepository.findById(userId).orElseThrow(() -> new ComponentNotFoundException("ID:" + userId + " USER_NOT_FOUND"));
+
+        return new UsernamePasswordAuthenticationToken(principal, token, authorities);
     }
 
 }
