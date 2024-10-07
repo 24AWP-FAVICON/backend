d9950f9db0c45742f5093c807bf1c06f91874d53
Minjeong Kim <101111603+Mingguriguri@users.noreply.github.com>
2024-10-07 17:49:23 +0900

    fix: JWT서비스 관련 코드 제거

--- src/main/java/com/example/demo/controller/messenger/MessageController.java
+++ src/main/java/com/example/demo/controller/messenger/MessageController.java
@@ -1,6 +1,5 @@
 package com.example.demo.controller.messenger;
 
-
 import com.example.demo.entity.users.user.User;
 import com.example.demo.service.messenger.ChatMessageService;
 import lombok.RequiredArgsConstructor;
@@ -9,10 +8,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-
 
 /**
  * 이 컨트롤러 클래스는 메시지 관련 API 요청을 처리합니다.
@@ -30,13 +25,9 @@ public class MessageController {
      * 사용자가 특정 채팅방에 참여하면 해당 채팅방의 모든 메시지를 읽음으로 처리합니다.
      *
      * @param roomId 읽음 처리할 채팅방의 ID
-     * @param request HttpServletRequest 객체 (JWT 검증에 사용)
-     * @param response HttpServletResponse 객체 (JWT 검증에 사용)
      */
     @PutMapping("/messages/read/{roomId}")
     public void markMessagesAsRead(@PathVariable Long roomId,
-                                   HttpServletRequest request,
-                                   HttpServletResponse response,
                                    Authentication authentication) {
         String userId = ((User) authentication.getPrincipal()).getUserId();
         chatMessageService.markMessagesAsRead(roomId, userId);
